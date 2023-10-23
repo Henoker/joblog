83d5d83068ae2d4b7dd94b699da044354108b0bc
Henoker <hennybany@gmail.com>
2023-10-23 10:46:51 +0300

    feat: permission classes with custom permissions

--- backend/job/views.py
+++ backend/job/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
 from .models import Job
 from .serializers import JobSerializer
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 class JobList(generics.ListCreateAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Job.objects.all()
     serializer_class = JobSerializer
     
 class JobDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = Job.objects.all()
     serializer_class = JobSerializer
     
